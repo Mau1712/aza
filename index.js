e34e80ad491567ee1e04c5e2725539856fa75468
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2023-11-11 00:16:35 -0300

    acl

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ profileCards.forEach(card => {
     });
 });
 
-// Función para cerrar el modal
+
 function closeCustomModal(modalId) {
     const modal = document.getElementById(modalId + "CustomModal");
     if (modal) {
@@ -92,3 +92,60 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+// Anclas ---------------------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+  
+        if (targetElement) {
+          console.log(`Haciendo clic en el enlace hacia ${targetId}`);
+  
+          let scrollConfig;
+  
+          // Configuraciones para pantallas pequeñas (máx. 480px)
+          if (window.innerWidth <= 480) {
+            scrollConfig = {
+              'anclaFirma': { top: 50, behavior: 'smooth', block: 'start' },
+              'anclaService': { top: -150, behavior: 'smooth' },
+              'anclaContacto': { top: -20, behavior: 'smooth' },
+              'anclaNosotros': { top: -100, behavior: 'smooth', block: 'start' }
+            };
+          } else {
+            // Configuraciones para pantallas más grandes
+            scrollConfig = {
+              'anclaFirma': { top: -50, behavior: 'smooth', block: 'start' },
+              'anclaService': { top: -180, behavior: 'smooth' },
+              'anclaContacto': { top: -30, behavior: 'smooth' },
+              'anclaNosotros': { top: 20, behavior: 'smooth', block: 'start' }
+            };
+          }
+  
+          const config = scrollConfig[targetId] || { behavior: 'smooth' };
+  
+          if (config.top !== undefined) {
+            const rect = targetElement.getBoundingClientRect();
+            const offsetTop = window.scrollY + rect.top;
+            window.scrollTo({
+              top: offsetTop + config.top,
+              behavior: config.behavior
+            });
+          } else {
+            targetElement.scrollIntoView(config);
+          }
+  
+          console.log(`Desplazando hacia ${targetId}`);
+        }
+      });
+    });
+  });
+  
+  
+  
+  
