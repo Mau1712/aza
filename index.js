afaf3f17ffe05da8c5886c7feb5be7c2c969102b
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2023-11-17 13:51:26 -0300

    modifications

--- index.js
+++ index.js
@@ -54,19 +54,19 @@ profileCards.forEach(card => {
 
             const closeSpan = modal.querySelector(".closeCustomModal");
             if (closeSpan) {
-                closeSpan.addEventListener("click", function() {
+                closeSpan.addEventListener("click", function () {
                     closeCustomModal(modalId);
                 });
             }
 
-            document.addEventListener("click", function(event) {
+            document.addEventListener("click", function (event) {
                 if (closeModalClickOnBackground) {
-                    closeModalClickOnBackground.addEventListener("click", function() {
+                    closeModalClickOnBackground.addEventListener("click", function () {
                         closeCustomModal(modalId);
                     });
                 }
             });
-            
+
         }
     });
 });
@@ -82,12 +82,12 @@ function closeCustomModal(modalId) {
 
 // ---------------------------------------------
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasWithBothOptions'));
     var links = document.querySelectorAll(".navItemMobile .nav-link");
 
-    links.forEach(function(link) {
-        link.addEventListener("click", function() {
+    links.forEach(function (link) {
+        link.addEventListener("click", function () {
             offcanvas.hide();
         });
     });
@@ -95,6 +95,58 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // Anclas ---------------------------------------
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//         anchor.addEventListener('click', function (e) {
+//             e.preventDefault();
+
+//             const targetId = this.getAttribute('href').substring(1);
+//             const targetElement = document.getElementById(targetId);
+
+//             if (targetElement) {
+//                 console.log(`Haciendo clic en el enlace hacia ${targetId}`);
+
+//                 let scrollConfig;
+
+//                 // Configuraciones para pantallas pequeñas (máx. 480px)
+//                 if (window.innerWidth <= 480) {
+//                     scrollConfig = {
+//                         'anclaFirma': { top: 50, behavior: 'smooth', block: 'start' },
+//                         'anclaService': { top: -90, behavior: 'smooth' },
+//                         'anclaContacto': { top: -20, behavior: 'smooth' },
+//                         'anclaNosotros': { top: -100, behavior: 'smooth', block: 'start' }
+//                     };
+//                 } else {
+//                     // Configuraciones para pantallas más grandes
+//                     scrollConfig = {
+//                         'anclaFirma': { top: -170, behavior: 'smooth', block: 'start' },
+//                         'anclaService': { top: -170, behavior: 'smooth', block: 'center' },
+//                         'anclaContacto': { top: -30, behavior: 'smooth' },
+//                         'anclaNosotros': { top: -30, behavior: 'smooth', block: 'start' }
+//                     };
+//                 }
+
+//                 const config = scrollConfig[targetId] || { behavior: 'smooth' };
+
+//                 if (config.top !== undefined) {
+//                     const rect = targetElement.getBoundingClientRect();
+//                     const offsetTop = window.scrollY + rect.top;
+//                     window.scrollTo({
+//                         top: offsetTop + config.top,
+//                         behavior: config.behavior
+//                     });
+//                 } else {
+//                     targetElement.scrollIntoView(config);
+//                 }
+
+//                 console.log(`Desplazando hacia ${targetId}`);
+//             } else {
+//                 console.log(`El elemento con el ID ${targetId} no fue encontrado.`);
+//             }
+//         });
+//     });
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -109,7 +161,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 let scrollConfig;
 
-                // Configuraciones para pantallas pequeñas (máx. 480px)
                 if (window.innerWidth <= 480) {
                     scrollConfig = {
                         'anclaFirma': { top: 50, behavior: 'smooth', block: 'start' },
@@ -117,13 +168,33 @@ document.addEventListener('DOMContentLoaded', function () {
                         'anclaContacto': { top: -20, behavior: 'smooth' },
                         'anclaNosotros': { top: -100, behavior: 'smooth', block: 'start' }
                     };
-                } else {
-                    // Configuraciones para pantallas más grandes
+                } else if (window.innerWidth <= 800) {
                     scrollConfig = {
-                        'anclaFirma': { top: -50, behavior: 'smooth', block: 'start' },
+                        'anclaFirma': { top: -130, behavior: 'smooth', block: 'start' },
                         'anclaService': { top: -150, behavior: 'smooth', block: 'center' },
                         'anclaContacto': { top: -30, behavior: 'smooth' },
-                        'anclaNosotros': { top: -30, behavior: 'smooth', block: 'start' }
+                        'anclaNosotros': { top: -100, behavior: 'smooth', block: 'start' }
+                    };
+                } else if (window.innerWidth <= 1024) {
+                    scrollConfig = {
+                        'anclaFirma': { top: -130, behavior: 'smooth', block: 'start' },
+                        'anclaService': { top: -100, behavior: 'smooth', block: 'center' },
+                        'anclaContacto': { top: -30, behavior: 'smooth' },
+                        'anclaNosotros': { top: -10, behavior: 'smooth', block: 'start' }
+                    };
+                } else if (window.innerWidth <= 1366) {
+                    scrollConfig = {
+                        'anclaFirma': { top: -125, behavior: 'smooth', block: 'start' },
+                        'anclaService': { top: -100, behavior: 'smooth', block: 'center' },
+                        'anclaContacto': { top: -30, behavior: 'smooth' },
+                        'anclaNosotros': { top: -5, behavior: 'smooth', block: 'start' }
+                    };
+                } else {
+                    scrollConfig = {
+                        'anclaFirma': { top: -175, behavior: 'smooth', block: 'start' },
+                        'anclaService': { top: -170, behavior: 'smooth', block: 'center' },
+                        'anclaContacto': { top: -50, behavior: 'smooth' },
+                        'anclaNosotros': { top: -15, behavior: 'smooth', block: 'start' }
                     };
                 }
 
@@ -148,7 +219,30 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-  
-  
-  
-  
+
+
+
+$(document).ready(function () {
+    $("#carouselExampleFade").on("touchstart", function (event) {
+        const xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function (event) {
+            const xMove = event.originalEvent.touches[0].pageX;
+            const sensitivityInPx = 5;
+
+            if (Math.floor(xClick - xMove) > sensitivityInPx) {
+                $(this).carousel('next');
+            }
+            else if (Math.floor(xClick - xMove) < -sensitivityInPx) {
+                $(this).carousel('prev');
+            }
+        });
+        $("#carouselExampleFade").on("touchend", function () {
+            $(this).off("touchmove");
+        });
+    });
+});
+
+
+
+
+
